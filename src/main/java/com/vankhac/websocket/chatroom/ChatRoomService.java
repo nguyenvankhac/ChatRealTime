d56322f6d5b167475fb24313395397f826de9a0d
vankhac <vankhacnguyen03@gmail.com>
2025-05-04 11:43:59 +0700

    second commit

--- src/main/java/com/vankhac/websocket/chatroom/ChatRoomService.java
+++ src/main/java/com/vankhac/websocket/chatroom/ChatRoomService.java
@@ -1,8 +1,33 @@
 package com.vankhac.websocket.chatroom;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class ChatRoomService {
 
+    private final ChatRoomRepository chatRoomRepository;
+    public Optional<String> getchatRoomId(
+            String senderId,
+            String recipientId,
+            boolean createNewRoomIfNotExists
+    ) {
+        return chatRoomRepository.finBySenderIdAndRecipientId(senderId,recipientId)
+                .map(ChatRoom::getChatId)
+                .or(() ->{
+                    if (createNewRoomIfNotExists){
+                        var chatId = createChat(senderId , recipientId);
+                    }
+                    return Optional.empty();
+                });
+
+    }
+
+    private String createChat(String senderId, String recipientId) {
+        var chatId = String.format("%s_%s",senderId, recipientId);
+        return null;
+    }
 }
