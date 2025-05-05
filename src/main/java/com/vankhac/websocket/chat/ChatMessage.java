b8d2b5b42bc146007a7f49c3aa1113bfe95db4d3
vankhac <vankhacnguyen03@gmail.com>
2025-05-05 16:58:12 +0700

    5/5 commit

--- src/main/java/com/vankhac/websocket/chat/ChatMessage.java
+++ src/main/java/com/vankhac/websocket/chat/ChatMessage.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,4 +17,7 @@ public class ChatMessage {
     private String id;
     private String chatId;
     private String senderId;
+    private String recipientId;
+    private String content;
+    private Date timestamp;
 }
---
+++ src/main/java/com/vankhac/websocket/chat/ChatMessageService.java
@@ -0,0 +1,32 @@
+package com.vankhac.websocket.chat;
+
+import com.vankhac.websocket.chatroom.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+
+    private final ChatMessageRepository chatMessageRepository;
+    private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage){
+        var chatId = chatRoomService.getchatRoomId(
+                chatMessage.getSenderId(),
+                chatMessage.getRecipientId(),
+                true
+        ).orElseThrow();
+        chatMessage.setChatId(chatId);
+        chatMessageRepository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getchatRoomId(senderId, recipientId, false);
+        return chatId.map(chatMessageRepository::findByChatId).orElse(new ArrayList<>());
+    }
+}
